cf161b816cd136bb99be0e7f6efcffd9fe064985
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2025-09-14 10:04:07 +0530

    Time: 0 ms (100%), Space: 23.6 MB (5.91%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int n=r.size();
+        vector<int>left(n),right(n);
+        if(n==0)return 0;
+        left[0]=1,right[n-1]=1;
+        for(int i=1;i<n;i++){
+            if(r[i]>r[i-1]){
+                left[i]=left[i-1]+1;
+            }
+            else{
+                left[i]=1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(r[i]>r[i+1]){
+                right[i]=right[i+1]+1;
+            }else{
+                right[i]=1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=max(left[i],right[i]);
+        }
+        return sum;
+    }
+};
