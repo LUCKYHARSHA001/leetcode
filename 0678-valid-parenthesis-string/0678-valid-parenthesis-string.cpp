83a4e4d39d8e577020052237eb4a731abafd02a4
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2025-09-14 07:38:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.87%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n=s.size();int mini=0,maxi=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                mini++;
+                maxi++;
+            }
+            else if(s[i]==')'){
+                mini--;
+                maxi--;
+            }
+            else{
+                mini--;
+                maxi++;
+            }
+            if(mini<0)mini=0;
+            if(maxi<0)return false;
+        }
+        return mini==0;
+    }
+};
