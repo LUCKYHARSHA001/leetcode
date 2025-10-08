fa9e22b5e1bc0e9361cb72c923f8b5c0da8cefb3
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2025-10-08 18:21:51 +0530

    Time: 51 ms (22.04%), Space: 128.2 MB (53.93%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -2,24 +2,14 @@ class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         vector<int>res;
-        int var=0;
         sort(potions.begin(),potions.end());
-        for(int i=0;i<spells.size();i++){
-           for(int j=0;j<potions.size();j++){
-            if(potions[j]*spells[i]>=success){
-                cout<<potions[j]*spells[i]<<" ";
-                var=potions.size()-j;
-                break;
-            }
-           }
-           if(var){
-           res.push_back(var);
-           var=0;
-           }else{
-            res.push_back(0);
-           }
+        int m=potions.size();
+        for(int spell:spells){
+            long long mid=(success+spell-1)/spell;
+            auto it=lower_bound(potions.begin(),potions.end(),mid);
+            int count=distance(it,potions.end());
+            res.push_back(count);
         }
         return res;
-
     }
 };
